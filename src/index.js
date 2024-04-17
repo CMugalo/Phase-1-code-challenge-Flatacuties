a361aa39e0c009b8b4f735d56765b13c7ddcb39d
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-04-17 23:31:51 +0300

    updated event listeners

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  // Building elements and DOM Render Functions
 
-// Building elements and DOM Render Functions
+  function renderOneCharacter(character) {
+    // Add character names to DOM
+    let card = document.createElement("span");
+    card.textContent = character.name;
+    document.querySelector("#character-bar").appendChild(card);
 
-function renderOneCharacter(character) {
-  let card = document.createElement("span");
-  card.textContent = `${character.name}`;
+    // Add event listener on click
+    card.addEventListener("click", (e) => {
+      e.preventDefault;
+      let gifOnDom = document.querySelector("#image");
+      gifOnDom.src = character.image;
+      let gifName = document.querySelector("#name");
+      gifName.textContent = character.name;
+      let gifVotes = document.querySelector("#vote-count");
+      gifVotes.textContent = character.votes;
+    });
+  }
 
-  // Add character names to DOM
-  document.querySelector("#character-bar").appendChild(card);
-}
+  // Fetch request for the character resources
+  function getAllCharacters() {
+    fetch("http://localhost:3000/characters")
+      .then((res) => res.json())
+      .then((characters) =>
+        characters.forEach((character) => renderOneCharacter(character))
+      );
+  }
 
-// Fetch request for the character resources
-function getAllCharacters() {
-  fetch("http://localhost:3000/characters")
-    .then((res) => res.json())
-    .then((characters) =>
-      characters.forEach((character) => renderOneCharacter(character))
-    );
-}
+  // Get character data, add them inside the div with span tags, and load them to the DOM
 
-// Get character data, add them inside the div with span tags, and load them to the DOM
-
-function initialize() {
-  getAllCharacters();
-  // characters.forEach((character) => renderOneCharacter(character));
-}
-initialize();
- 
+  function initialize() {
+    getAllCharacters();
+  }
+  initialize();
+});
