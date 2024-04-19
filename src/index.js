230ad0b7afe50538c46d7e1fe4e2a038f13b5971
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-04-19 06:40:31 +0300

    updated DOM elements

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
 // Your code here
+
 document.addEventListener("DOMContentLoaded", () => {
   // Building elements and DOM Render Functions
 
   function renderOneCharacter(character) {
     // Add character names to DOM
-    let card = document.createElement("span");
+    const card = document.createElement("span");
     card.textContent = character.name;
     document.querySelector("#character-bar").appendChild(card);
 
-    // Add event listener on click
+    // Adding click event listener
     card.addEventListener("click", (e) => {
       e.preventDefault;
-      let gifOnDom = document.querySelector("#image");
+      const gifOnDom = document.querySelector("#image");
       gifOnDom.src = character.image;
-      let gifName = document.querySelector("#name");
+      const gifName = document.querySelector("#name");
       gifName.textContent = character.name;
-      let gifVotes = document.querySelector("#vote-count");
+      const gifVotes = document.querySelector("#vote-count");
       gifVotes.textContent = character.votes;
     });
   }
+  /////////////////////////////////////////////////
+  const votingForm = document.querySelector("#votes-form");
+  votingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    totalVotes(e.target.votes.value);
+    votingForm.reset();
+  });
+  ///////////////////////////////////////////////////
+
+  // Displaying votes cast
+
+  ///////////////////////////////////////////////////
+  function totalVotes(animalVotes) {
+    console.log(animalVotes);
+    const displayVotes = document.querySelector("#vote-count");
+    displayVotes.textContent = votesCast(animalVotes);
+  }
+
+  function votesCast(newVotes) {
+    if (isNaN(newVotes)) {
+      return alert("Votes must be a number!");
+    } else {
+      return newVotes;
+    }
+  }
+  /////////////////////////////////////////////////
 
   // Fetch request for the character resources
+
   function getAllCharacters() {
     fetch("http://localhost:3000/characters")
       .then((res) => res.json())
